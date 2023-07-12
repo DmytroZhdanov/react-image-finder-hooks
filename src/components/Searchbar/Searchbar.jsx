c22bd36b12a75646df6a59e4fc8ac8f1df29ddd8
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-12 18:29:28 +0200

    Refactors Searchbar from class to hook

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,18 +1,15 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import { ImSearch } from 'react-icons/im';
 import { Container, Input, SearchButton, SearchForm } from './Searchbar.styled';
 
-export class Searchbar extends Component {
-  state = {
-    input: '',
-  };
+export const Searchbar = ({ onSearch }) => {
+  const [input, setInput] = useState('');
 
-  handleSubmit = async e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
     const { value } = e.target.elements.searchQuery;
-    const { onSearch } = this.props;
     const query = value.trim();
 
     if (query === '') {
@@ -22,34 +19,30 @@ export class Searchbar extends Component {
     onSearch(query);
   };
 
-  handleInputChange = e => {
-    this.setState({ input: e.target.value });
+  const handleInputChange = e => {
+    setInput(e.target.value);
   };
 
-  render() {
-    return (
-      <Container>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchButton type="submit">
-            <ImSearch />
-          </SearchButton>
-          <Input
-            type="text"
-            name="searchQuery"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.input}
-            onChange={this.handleInputChange}
-          />
-        </SearchForm>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchButton type="submit">
+          <ImSearch />
+        </SearchButton>
+        <Input
+          type="text"
+          name="searchQuery"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={input}
+          onChange={handleInputChange}
+        />
+      </SearchForm>
+    </Container>
+  );
+};
 
 Searchbar.propTypes = {
   onSearch: PropTypes.func.isRequired,
-  // setStatus: PropTypes.func.isRequired,
-  // resetImages: PropTypes.func.isRequired,
 };
