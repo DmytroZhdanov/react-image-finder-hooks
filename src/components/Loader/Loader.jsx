3deca4b8aae2c0a46c985449eac7f3dd6bc1a457
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-13 12:31:16 +0200

    Refactors Loader from class to hook

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,17 @@
-import { Component } from "react";
 import { ThreeDots } from 'react-loader-spinner';
 import { Container } from "./Loader.styled";
 
-
-export class Loader extends Component {
-  render() {
-    return (
-      <Container>
-        <ThreeDots
-          height="80"
-          width="80"
-          radius="9"
-          color="#3f51b5"
-          ariaLabel="three-dots-loading"
-          visible={true}
-        />
-      </Container>
-    );
-  }
+export const Loader = () => {
+  return (
+    <Container>
+      <ThreeDots
+        height="80"
+        width="80"
+        radius="9"
+        color="#3f51b5"
+        ariaLabel="three-dots-loading"
+        visible={true}
+      />
+    </Container>
+  );
 }
