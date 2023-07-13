307e16efc2dada61fd2583742450dc4b0c1e844b
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-13 12:39:43 +0200

    Refactors Button from class to hook

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,9 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { Btn } from './Button.styled';
 
-export class Button extends Component {
-  handleClick = () => {
-    this.props.onClick();
-  };
-
-  render() {
-    return <Btn onClick={this.handleClick}>Load more</Btn>;
-  }
-}
+export const Button = ({ onClick }) => {
+  return <Btn onClick={()=>{onClick()}}>Load more</Btn>;
+};
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
