67db84cf7353a155b37cea2ca3d8f8a6e4a9a68a
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-07-13 12:36:30 +0200

    Refactors ImageGallery from class to hook

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,27 +1,22 @@
 import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Component } from 'react';
 import { Gallery } from './ImageGallery.styled';
 
-export class ImageGallery extends Component {
-  render() {
-    const { images, setModalImg } = this.props;
-
-    return (
-      <Gallery>
-        {images.map(({ webformatURL, tags, largeImageURL }, index) => (
-          <ImageGalleryItem
-            key={index}
-            modalUrl={largeImageURL}
-            url={webformatURL}
-            tags={tags}
-            setModalImg={setModalImg}
-          />
-        ))}
-      </Gallery>
-    );
-  }
-}
+export const ImageGallery = ({ images, setModalImg }) => {
+  return (
+    <Gallery>
+      {images.map(({ webformatURL, tags, largeImageURL }, index) => (
+        <ImageGalleryItem
+          key={index}
+          modalUrl={largeImageURL}
+          url={webformatURL}
+          tags={tags}
+          setModalImg={setModalImg}
+        />
+      ))}
+    </Gallery>
+  );
+};
 
 ImageGallery.propTypes = {
   images: PropTypes.arrayOf(
